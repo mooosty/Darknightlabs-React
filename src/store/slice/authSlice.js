a4f7f886617785125a5d9034f73d039fc1bc3144
bhumi1908 <bhumigangani1908@gmail.com>
2024-11-12 19:03:28 +0530

    Token change

--- src/store/slice/authSlice.js
+++ src/store/slice/authSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIwLCJpYXQiOjE3MzEzOTExNjUsImV4cCI6MTczMzk4MzE2NX0.5mFJ_6QusnqM86nljTnKwhtpOUbDR25elCBFnWYtFco',
+    // token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIwLCJpYXQiOjE3MzEzOTExNjUsImV4cCI6MTczMzk4MzE2NX0.5mFJ_6QusnqM86nljTnKwhtpOUbDR25elCBFnWYtFco',
+    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTczMDQ1NjQwMSwiZXhwIjoxNzMzMDQ4NDAxfQ.ESfezUIJjA4tO-t-lnH6PWFIkUDuJqqyCqLejY72e2U',
     userId: "66d803bb836702a60484f35e",
     name:'Test',
     email:'test123@gmail.com'
