15544628bfb41e06f17dbadb721278f040ef68b6
bhumi1908 <bhumigangani1908@gmail.com>
2024-12-11 15:34:09 +0530

    Profile page design and functional fixes

--- src/pages/user-profile/UserProfile.jsx
+++ src/pages/user-profile/UserProfile.jsx
@@ -41,7 +41,7 @@ const UserProfile = () => {
   const dispatch = useDispatch();
   const userData = useSelector((state) => state.auth);
   const { userDetails } = useSelector((state) => state.user);
-  console.log(userData);
+
   const [active, setActive] = useState("INFORMATION");
   const [isEditMode, setIsEditMode] = useState(false);
   const [isLoginPopupOpen, setIsLoginPopupOpen] = useState(false);
@@ -49,26 +49,23 @@ const UserProfile = () => {
   const [image, setImage] = useState("");
   const [isImageChange, setIsImageChange] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-
   const [userProjects, setUserProjects] = useState([]);
   const [addNewProject, setAddNewProject] = useState(false);
 
-  /********************* */
+
   useEffect(() => {
     const fetchProjects = async () => {
       setIsLoading(true);
       try {
-       
-        const response = await axios.get(`https://winwinsocietyweb3.com/api/userprojects/all/${userData.userId}`, {
+        const response = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/userprojects/all/${userData.userId}`, {
           headers: {
             Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjZmNjg4Y2I0LTk3MmItNDZhNy1iZWMwLTJjOTEyNTVlYjUyMyJ9.eyJraWQiOiI2ZjY4OGNiNC05NzJiLTQ2YTctYmVjMC0yYzkxMjU1ZWI1MjMiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUxNzMiLCJpc3MiOiJhcHAuZHluYW1pY2F1dGguY29tL2QxZmZjZWVhLTg5Y2UtNGM4Zi1iYTMyLTBiODAyZmFiODgyMiIsInN1YiI6IjM4NDM3NWQ1LTJiNjUtNDQzMC1iZTQ4LWYwYzE4N2Q0YTg5ZiIsInNpZCI6Ijk2YWJhMGJiLTU3YWYtNGU4YS1hOTkyLTc0ZTYzNmNhZGU4YSIsImVtYWlsIjoic2Ftc2F3bG8xMDBAZ21haWwuY29tIiwiZW52aXJvbm1lbnRfaWQiOiJkMWZmY2VlYS04OWNlLTRjOGYtYmEzMi0wYjgwMmZhYjg4MjIiLCJsaXN0cyI6W10sIm1pc3NpbmdfZmllbGRzIjpbXSwidmVyaWZpZWRfY3JlZGVudGlhbHMiOlt7ImFkZHJlc3MiOiIweEM5QTZjZjZjZTc4RUI0OTkxQjFkMUFBNTgwOTJjNjBiNUVkNkZEQTgiLCJjaGFpbiI6ImVpcDE1NSIsImlkIjoiYzY0NjFlZjAtNmMxZC00ZjdhLWI4NDUtODEzMTg5NDIzYTU5IiwibmFtZV9zZXJ2aWNlIjp7fSwicHVibGljX2lkZW50aWZpZXIiOiIweEM5QTZjZjZjZTc4RUI0OTkxQjFkMUFBNTgwOTJjNjBiNUVkNkZEQTgiLCJ3YWxsZXRfbmFtZSI6InR1cm5rZXloZCIsIndhbGxldF9wcm92aWRlciI6ImVtYmVkZGVkV2FsbGV0Iiwid2FsbGV0X3Byb3BlcnRpZXMiOnsidHVybmtleVN1Yk9yZ2FuaXphdGlvbklkIjoiMmI1M2RmODktNWM0My00MzNmLTg2Y2MtZGJmZDg5ZTEyNDNiIiwidHVybmtleUhEV2FsbGV0SWQiOiJkMmVlOWMwZC0wNTkwLTVmMmEtYjMwMi1kODFiYjZjMTA4NzMiLCJpc0F1dGhlbnRpY2F0b3JBdHRhY2hlZCI6ZmFsc2UsInR1cm5rZXlVc2VySWQiOiI0Zjk2MjE3My0wMzAwLTRiODAtODBhOC05MzRiMmM5NTRiN2MiLCJpc1Nlc3Npb25LZXlDb21wYXRpYmxlIjpmYWxzZSwidmVyc2lvbiI6IlYxIn0sImZvcm1hdCI6ImJsb2NrY2hhaW4iLCJsYXN0U2VsZWN0ZWRBdCI6IjIwMjQtMTItMDZUMTU6MTI6MjguOTUzWiIsInNpZ25JbkVuYWJsZWQiOmZhbHNlfSx7ImVtYWlsIjoic2Ftc2F3bG8xMDBAZ21haWwuY29tIiwiaWQiOiJmNDU3NzJkNy0yM2FmLTQ3ZmQtYmQ1YS1lZjJjMGRmNzcxNmQiLCJwdWJsaWNfaWRlbnRpZmllciI6InNhbXNhd2xvMTAwQGdtYWlsLmNvbSIsImZvcm1hdCI6ImVtYWlsIiwic2lnbkluRW5hYmxlZCI6dHJ1ZX0seyJpZCI6IjZlZGM1NmM5LTQwN2QtNDlmYi04NGQ4LTZmMmI4ZTI5NDQxZSIsInB1YmxpY19pZGVudGlmaWVyIjoiTGFoY2VuIiwiZm9ybWF0Ijoib2F1dGgiLCJvYXV0aF9wcm92aWRlciI6InR3aXR0ZXIiLCJvYXV0aF91c2VybmFtZSI6ImxhaGNlbnJhaGxhb3VpIiwib2F1dGhfZGlzcGxheV9uYW1lIjoiTGFoY2VuIiwib2F1dGhfYWNjb3VudF9pZCI6IjEyMTQzMjk3NjIzNzE1NTk0MjQiLCJvYXV0aF9hY2NvdW50X3Bob3RvcyI6WyJodHRwczovL3Bicy50d2ltZy5jb20vcHJvZmlsZV9pbWFnZXMvMTQ2MDMwMjE5OTQ5MDI5Mzc2Ni8yeDhTYUE4Tl9ub3JtYWwuanBnIl0sIm9hdXRoX2VtYWlscyI6W10sIm9hdXRoX21ldGFkYXRhIjp7ImlkIjoiMTIxNDMyOTc2MjM3MTU1OTQyNCIsIm5hbWUiOiJMYWhjZW4iLCJwdWJsaWNfbWV0cmljcyI6eyJmb2xsb3dlcnNfY291bnQiOjIsImZvbGxvd2luZ19jb3VudCI6MTUsInR3ZWV0X2NvdW50IjowLCJsaXN0ZWRfY291bnQiOjAsImxpa2VfY291bnQiOjIsIm1lZGlhX2NvdW50IjowfSwicHJvZmlsZV9pbWFnZV91cmwiOiJodHRwczovL3Bicy50d2ltZy5jb20vcHJvZmlsZV9pbWFnZXMvMTQ2MDMwMjE5OTQ5MDI5Mzc2Ni8yeDhTYUE4Tl9ub3JtYWwuanBnIiwiZGVzY3JpcHRpb24iOiIiLCJ1c2VybmFtZSI6ImxhaGNlbnJhaGxhb3VpIn0sInNpZ25JbkVuYWJsZWQiOnRydWV9XSwibGFzdF92ZXJpZmllZF9jcmVkZW50aWFsX2lkIjoiNmVkYzU2YzktNDA3ZC00OWZiLTg0ZDgtNmYyYjhlMjk0NDFlIiwiZmlyc3RfdmlzaXQiOiIyMDI0LTEyLTA1VDE1OjAzOjExLjAxOFoiLCJsYXN0X3Zpc2l0IjoiMjAyNC0xMi0wNlQxNToxMjoyOC41NzNaIiwibmV3X3VzZXIiOmZhbHNlLCJtZXRhZGF0YSI6e30sInZlcmlmaWVkQ3JlZGVudGlhbHNIYXNoZXMiOnsiYmxvY2tjaGFpbiI6ImQ0NzQxYmJjMjA3OWVlOTI4M2EzNmExMGI4ODg3ODdlIiwiZW1haWwiOiI0MzVjY2FiZTEzNmFlNTk3NWNhYTE3ZWVlM2NlNTI0NCIsIm9hdXRoIjoiNjliOWMwYjliMmRmMDk1NWU3NjMwNmM0YTQ0NjNiNjIifSwiaWF0IjoxNzMzNDk3OTQ5LCJleHAiOjE3MzYwODk5NDl9.KoZZuNpDFD8nuGvtz--5qbuUQuXpqHxG5Vfnph-piOZb62eR7DvbP_pJAq48FU4WonGSix75CNeuwzbJhuu2pv3ihgCoPFb77mSRWFBUrgQIBnbYCx_wcVQk-I20_gugsR-pVoGbhpA4EMweOeJ3rSWmQ9TtFJhkCIHgVj6hYr4wVzxDq0pbWYgN-nIS6hGF6l4rMQvq_5aQY9ySLGNstvo9spW6H52R9UuzMSv2V54DX_0RixZr-yMSJhZivUUU8f53DW5iGktZJH5KsN5egzmHR5Yp3JUMy2NAc2cRPHXOAUZ1SjKKqOUsyBFRO1H-w1KjJU1NoH-egFlv7xU9DPtv8i_Ls9SyokRS7WOsgLI64ah29dSDcIJf5k0C1Ovh12qpQynE0JM8J7ZzgkW0-sBTDlcL9wZKzXMI58t0fHUG9-mcjoBtfecakWKwe2xfoXs_3cn3lhyKHQD0I0bxDiz6v_YpCC84i7Xk3N3Fn1_8SOzg3HKFbyTfUtlKZPpWdlK_RltQdDTjivBpz6v-irNZChvAljrczwiw8fln62mB49VAuT2XBI_sLVAalr1CdJCFnVg3TEe53Q-hznT9pxdUF_9QP1b6aTs0X2N60B4Tk3TOfFGgt0Njn6usbkR63fh676v5fFL5y3aibP8JSNDlYRCdowmjAz8oVoMGblc`,
           },
         });
-       
-        console.log("response.data"); 
-        console.log(response.data.data);
-        setUserProjects(response.data.data);
-        // dispatch(setUserProjects(response.data));
+
+        console.log("response.data");
+        console.log(response);
+        setUserProjects(response.data.data ?? []);
       } catch (error) {
         console.log(error.message);
       } finally {
@@ -77,6 +74,8 @@ const UserProfile = () => {
     };
     fetchProjects();
   }, [dispatch]);
+
+
   /********************* */
   const handleActive = (key) => {
     setActive(key);
@@ -245,9 +244,9 @@ const UserProfile = () => {
                     <div className="profile_upload_profile">
                       <img
                         src={userData?.profile_picture}
-                        // src={userDetails?.profile_picture === '' || userData?.profile_picture || !userDetails?.profile_picture ? defaultImg : userDetails?.profile_picture}
-                        // alt=""
-                        // onError={(e) => e.target.src = defaultImg}
+                      // src={userDetails?.profile_picture === '' || userData?.profile_picture || !userDetails?.profile_picture ? defaultImg : userDetails?.profile_picture}
+                      // alt=""
+                      // onError={(e) => e.target.src = defaultImg}
                       />
                     </div>
                   </div>
--- src/pages/user-profile/project-manager/ProjectsUser.jsx
+++ src/pages/user-profile/project-manager/ProjectsUser.jsx
@@ -1,51 +1,30 @@
 import "./projectmanager.scss";
-import debounce from "lodash.debounce";
-import { useNavigate } from "react-router-dom";
 import { formatDate } from "../../../utils/helper/helper";
 import { useSelector, useDispatch } from "react-redux";
 import useNoScroll from "../../../utils/hooks/useNoScroll";
-import { useCallback, useEffect, useState } from "react";
-import { synergyAnglesOptions } from "../../../utils/constants/options";
-import { GridIcon, ListIcon, TableStatusIcon, InfiniteIcon, MoreIcon } from "../../../utils/SVGs/SVGs";
-import { searchIcon, filterIcon, trashIcon, addIcon, closeIcon } from "../../../utils/constants/images";
-import { getProjectsAPI, deleteProjectAPI, updateProjectAPI, getMemberApi } from "../../../api-services/projectApis";
+import { useEffect, useState } from "react";
+import { addIcon } from "../../../utils/constants/images";
+import { getProjectsAPI, deleteProjectAPI, getMemberApi } from "../../../api-services/projectApis";
 import {
-  ProjectManagerTableLayout,
-  ProjectManagerGridLayout,
   CreateSynergySteps,
-  Select,
   DeleteConfirmPopup,
   SynergieaCreatedSuccessfullyPopup,
-  BottomMenu,
   Loader,
 } from "../../../components";
 import TableLayout from "./project-manager-component/TableLayout";
-import img from "../../../assets/model-frame.png";
+import PropTypes from "prop-types";
 
 const ProjectsUser = ({ userProjects, setAddNewProject, handleActive, active }) => {
-  
-  const [activeLayout, setActiveLayout] = useState("TABLE");
-  const [isFilterOpen, setIsFilterOpen] = useState(false);
 
   const [selectedProjects, setSelectedProjects] = useState([]);
   const [selectedProjectForSynergy, setSelectedProjectForSynergy] = useState(null);
   const [isDeleteConfirmPopupOpen, setIsDeleteConfirmPopupOpen] = useState(false);
   const [isMultiDltConfirmPopupOpen, setIsMultiDltConfirmPopupOpen] = useState(false);
   const [dltId, setDltId] = useState(null);
-  const [filter, setFilter] = useState({
-    synergyAngleValue: "",
-    status: "",
-    sortBy: "",
-    type: "",
-    searchBy: "",
-  });
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const data = useSelector((state) => state.project.projects);
   const projectApiLoading = useSelector((state) => state.project.isLoading);
-
-  const [initialProject, setInitialProject] = useState([]);
   const [filterProject, setFilterProject] = useState([]);
   const [synergies, setSynergies] = useState({
     synergyName: "",
@@ -54,14 +33,6 @@ const ProjectsUser = ({ userProjects, setAddNewProject, handleActive, active }) 
   const [createSynergyStep, setCreateSynergyStep] = useState(0);
   const [createSynergySuccessPopup, setCreateSynergySuccessPopup] = useState(false);
   useNoScroll([isDeleteConfirmPopupOpen, createSynergySuccessPopup, isMultiDltConfirmPopupOpen]);
-
-  // const handleActive = (key) => {
-  //   setActiveLayout(key);
-  // };
-
-  const handleFilterOpen = () => {
-    setIsFilterOpen(!isFilterOpen);
-  };
 
   const handleDelete = (projectId) => {
     dispatch(
@@ -104,95 +75,26 @@ const ProjectsUser = ({ userProjects, setAddNewProject, handleActive, active }) 
   };
 
   useEffect(() => {
-    let data = initialProject;
-    if (filter.synergyAngleValue !== "") {
-      const filterArr = data.filter((project) => {
-        return (
-          project.synergiesAngles.findIndex((synergy) => {
-            return synergy.label === filter.synergyAngleValue;
-          }) !== -1
-        );
-      });
-      data = [...filterArr];
-    }
-    if (filter.status !== "") {
-      const filterArr = data.filter((project) => {
-        if (filter.status === "Featured") return project.isFeatured;
-        else {
-          return !project.isFeatured;
-        }
-      });
-      data = [...filterArr];
-    }
-    if (filter.type !== "") {
-      const filterArr = data.filter((project) => {
-        return project.type?.findIndex((typeVal) => typeVal === `#${filter.type}`) !== -1;
-      });
-      data = [...filterArr];
-    }
-    if (filter.sortBy !== "") {
-      if (filter.sortBy === "name") {
-        const filterArr = data.sort((project1, project2) => {
-          const projectName1 = project1.projectName.toLowerCase();
-          const projectName2 = project2.projectName.toLowerCase();
-
-          return projectName1.localeCompare(projectName2);
-        });
-
-        data = [...filterArr];
-      } else if (filter.sortBy === "date") {
-        const filterArr = data.sort((project1, project2) => {
-          var date1 = project1.date.split("/").reverse().join();
-          var date2 = project2.date.split("/").reverse().join();
-          return date1 < date2 ? -1 : date1 > date2 ? 1 : 0;
-        });
-        data = [...filterArr];
-      } else if (filter.sortBy === "description") {
-        const filterArr = data.sort((project1, project2) => {
-          const description1 = project1.description.trim()?.toLowerCase();
-          const description2 = project2.description.trim()?.toLowerCase();
-
-          if (description1 < description2) {
-            return -1;
-          }
-          if (description1 > description2) {
-            return 1;
-          }
-          return 0;
-        });
-        data = [...filterArr];
-      }
-    }
-
-    if (filter.searchBy !== "") {
-      const searchKeyword = filter.searchBy.toLowerCase();
-      data = data.filter(
-        (project) =>
-          project.projectName.toLowerCase().includes(searchKeyword) ||
-          project.description.toLowerCase().includes(searchKeyword)
-      );
-    }
-
-    setFilterProject([...data]);
-  }, [filter]);
-
-  useEffect(() => {
     const projectData = [
       ...userProjects.map((project, index) => {
         let tags = project.project_info?.split("#") || [];
         tags = tags.filter((tag) => tag).map((tag) => `#${tag}`);
 
-        let synergy_angles = Object.keys(project.synergy_angles)
-          .map((key) => (project.synergy_angles[key] ? { label: project.synergy_angles[key] } : null))
-          .filter((item) => item);
+        const synergyAngleData = project.synergy_angles ? JSON.parse(project.synergy_angles) : {}
+        let synergyAngle = []
+        Object.keys(synergyAngleData ?? {}).forEach((key) => {
+          if (synergyAngleData[key]) {
+            synergyAngle.push({ label: synergyAngleData[key] })
+          }
+        })
 
         const row = {
           key: index,
           checked: false,
           projectName: project.project_name,
-          role : project.job_desc,
+          role: project.job_desc,
           synergyImg: project.image ?? "",
-          synergiesAngles: synergy_angles,
+          synergiesAngles: synergyAngle,
           type: tags,
           isFeatured: project.featured,
           date: formatDate(project.date),
@@ -203,10 +105,8 @@ const ProjectsUser = ({ userProjects, setAddNewProject, handleActive, active }) 
         return row;
       }),
     ];
-    setInitialProject([...projectData]);
-    console.log("projectData", projectData);
     setFilterProject([...projectData]);
-  }, [data , userProjects ]);
+  }, [data, userProjects]);
 
   useEffect(() => {
     if (data.length === 0)
@@ -221,213 +121,50 @@ const ProjectsUser = ({ userProjects, setAddNewProject, handleActive, active }) 
 
   return (
     <>
-      <div className="content_header">
-        <div className="content_left">
+      <div className="project_user_content_header">
+        <div className="project_user_content_left">
           <h2>Profile</h2>
         </div>
-        <div className="content_right">
+        <div className="project_user_content_right">
           <a href="#">Darknight Labs</a>
         </div>
       </div>
-      <div className="project_page_data">
+      <div className="project_user_page_data">
         <div className="page_data">
-          {/* border-radius: 20px;
-            background-color: #191917;
-            background-image: url("../../assets/model-frame.png");
-            background-repeat: no-repeat;
-            background-position-x: 10px;
-            background-position-y: 10px;
-            min-height: 540px;
-            height: calc(100% - 80px); */}
-
-          <div className="project_page_header">
+          <div className="header_button">
             <div
-              style={{
-                backgroundImage: `url(${img})`,
-              backgroundRepeat: "no-repeat",
-                marginTop: "10px",
-               }}
-              className="project_page_header_top"
+              className={`buttons ${active === "INFORMATION" ? "active" : ""}`}
+              onClick={() => handleActive("INFORMATION")}
             >
-              <div
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  flexDirection: window.innerWidth < 768 ? "column" : "row",
-                  flexWrap: window.innerWidth < 768 ? "nowrap" : "wrap",
-                  backgroundColor: "transparent",
-                  height: "100%",
-                  overflowY: "auto",
-                  color: "white",
-                  gap: "20px",
-                  flexDirection: "column",
-                  padding: "20px",
-                  boxSizing: "border-box",
-                  maxWidth: "calc(100vw - 30px)",
-                }}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    marginBottom: "25px",
-                    gap: "10px",
-                    justifyContent: "",
-                  }}
-                >
-                  <div
-                    style={{
-                      color: active !== "INFORMATION" ? "#ffffff80" : "white",
-                      padding: "15px 10px",
-                      display: "flex",
-                      justifyContent: "center",
-                      borderBottom: "1px solid #ffffff80",
-                      cursor: "pointer",
-                      alignItems: "center",
-                      textAlign: "center",
-                    }}
-                    onClick={() => handleActive("INFORMATION")}
-                  >
-                    PERSONAL INFORMATION
-                  </div>
-                  <div
-                    style={{
-                      color: active !== "INVOLVEMENT" ? "#ffffff80" : "white",
-                      padding: "15px 10px",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      borderBottom: "1px solid #ffffff80",
-                      cursor: "pointer",
-                      alignItems: "center",
-                      textAlign: "center",
-                    }}
-                    onClick={() => handleActive("INFORMATION")}
-                  >
-                    PROJECT INVOLVEMENT
-                  </div>
-                </div>
-
-                <div style={{ display: "flex", justifyContent: "flex-end", width: "100%" }}>
-                  <div className="project_page_header_button">
-                    <button
-                      className={`btn_gray `}
-                      onClick={() => {
-                        setAddNewProject(true);
-                      }}
-                    >
-                      Add New Project
-                      <img src={addIcon} alt="" />
-                    </button>
-                  </div>
-                </div>
-              </div>
+              PERSONAL INFORMATION
             </div>
-            <div className={`project_page_filter ${isFilterOpen ? "active" : ""}`}>
-              {/* <div className="angels">
-                <Select
-                  options={synergyAnglesOptions}
-                  placeholder={"All synergies angles"}
-                  onChange={(value) => {
-                    setSelectedProjects([]);
-                    if (value.value === "All") {
-                      setFilter({
-                        ...filter,
-                        synergyAngleValue: "",
-                      });
-                    } else {
-                      setFilter({
-                        ...filter,
-                        synergyAngleValue: value.value,
-                      });
-                    }
-                  }}
-                  showAllOption={true}
-                  allOptionText={"All synergies angles"}
-                />
-              </div> */}
-              <div className="type">
-                <Select
-                  options={[
-                    { label: "GAME", value: "GAME" },
-                    { label: "AI", value: "AI" },
-                    { label: "hey", value: "hey" },
-                  ]}
-                  onChange={(value) => {
-                    setSelectedProjects([]);
-                    if (value.value === "All") {
-                      setFilter({
-                        ...filter,
-                        type: "",
-                      });
-                    } else {
-                      setFilter({
-                        ...filter,
-                        type: value.value,
-                      });
-                    }
-                  }}
-                  placeholder={"All project types"}
-                  showAllOption={true}
-                  allOptionText={"All project types"}
-                />
-              </div>
-              <div className="status">
-                <Select
-                  options={[
-                    { label: "Featured", value: "Featured" },
-                    { label: "Non Featured", value: "Non Featured" },
-                  ]}
-                  placeholder={"All Statuses"}
-                  onChange={(value) => {
-                    setSelectedProjects([]);
-                    if (value.value === "All") {
-                      setFilter({
-                        ...filter,
-                        status: "",
-                      });
-                    } else {
-                      setFilter({
-                        ...filter,
-                        status: value.value,
-                      });
-                    }
-                  }}
-                  showAllOption={true}
-                  allOptionText={"All Statuses"}
-                />
-              </div>
-              <div className="sort">
-                <Select
-                  options={[
-                    { label: "Project Name", value: "name" },
-                    { label: "Description", value: "description" },
-                    { label: "Date", value: "date" },
-                  ]}
-                  placeholder={"Sort by"}
-                  onChange={(value) => {
-                    setSelectedProjects([]);
-                    setFilter({
-                      ...filter,
-                      sortBy: value.value,
-                    });
-                  }}
-                />
-              </div>
+            <div
+              className={`buttons ${active === "INVOLVEMENT" ? "active" : ""}`}
+              onClick={() => handleActive("INVOLVEMENT")}
+            >
+              PROJECT INVOLVEMENT
             </div>
           </div>
-          <div className="project_page_body">
-            {activeLayout === "TABLE" && (
-              <TableLayout
-                filterProject={filterProject}
-                selectedProjects={selectedProjects}
-                selectedProjectForSynergy={selectedProjectForSynergy}
-                createSynergyStep={createSynergyStep}
-                handleSelectProject={handleSelectProject}
-                setIsDeleteConfirmPopupOpen={setIsDeleteConfirmPopupOpen}
-                setDltId={setDltId}
-              />
-            )}
-            {activeLayout === "GRID" && <ProjectManagerGridLayout filterProject={filterProject} />}
+          <div className="project_user_page_header">
+            <div className="pagination">
+              <span>Project</span>
+            </div>
+            <button className="btn_gray add_project_btn" onClick={() => { setAddNewProject(true); }}>
+              Add New Project
+              <img src={addIcon} alt="" />
+            </button>
+          </div>
+
+          <div className="project_user_page_body">
+            <TableLayout
+              filterProject={filterProject}
+              selectedProjects={selectedProjects}
+              selectedProjectForSynergy={selectedProjectForSynergy}
+              createSynergyStep={createSynergyStep}
+              handleSelectProject={handleSelectProject}
+              setIsDeleteConfirmPopupOpen={setIsDeleteConfirmPopupOpen}
+              setDltId={setDltId}
+            />
           </div>
         </div>
       </div>
@@ -481,6 +218,13 @@ const ProjectsUser = ({ userProjects, setAddNewProject, handleActive, active }) 
       <Loader loading={projectApiLoading} />
     </>
   );
+};
+
+ProjectsUser.propTypes = {
+  userProjects: PropTypes.array.isRequired,
+  setAddNewProject: PropTypes.func.isRequired,
+  handleActive: PropTypes.func.isRequired,
+  active: PropTypes.bool.isRequired,
 };
 
 export default ProjectsUser;
